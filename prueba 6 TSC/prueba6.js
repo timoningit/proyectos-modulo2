f102a39c1e4415fb1e7f2f9468dd0cef0f7e9948
timoningit <info.tecno.timon@gmail.com>
2022-10-12 16:25:58 -0300

    tareas 1 al 6

---
+++ prueba 6 TSC/prueba6.js
@@ -0,0 +1,8 @@
+let nombre = "invitado";
+
+function soyUnaFuncionjs(mensaje) {
+    console.log("Aqui funciona javascript"+ nombre);
+}
+
+// array
+let list = [1,2,3];
---
+++ prueba 6 TSC/prueba6.ts
@@ -0,0 +1,12 @@
+let nombre: string = "invitado";
+
+function soyUnaFuncionts( mensaje : string) {
+    console.log("Aqui funciona typescript" + nombre);
+}
+
+// arrays
+let list: number[] = [1,2,3];
+
+let planetas: string[] = ["Mercurio","Venus","Tierra"];
+
+let vidaPosible: boolean[] = [false,false,true];
